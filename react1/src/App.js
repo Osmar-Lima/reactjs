875aa79f1ac34fefa48dcb7459d45c76ab7eca81
Osmar Lima <osmar.minhos@gmail.com>
2020-12-08 07:11:16 -0300

    add my first code

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// todo elemento é um componente que pode aparecer na tela.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+function welcome(){
+  return <h1>Ola, Osmar</h1>
 }
 
-export default App;
+
+function App(){
+
+  
+  return <div class="conteudo">
+
+    <welcome/>
+
+  </div>
+}
+export default App();
